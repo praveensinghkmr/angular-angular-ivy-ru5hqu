988522e3dae9b8b95d85d04f2ca57dc789f8874c
praveensinghkmr <praveensingh.kmr@gmail.com>
2022-01-05 15:42:20 +0530

    2 files modified

--- src/app/test1.component.ts
+++ src/app/test1.component.ts
@@ -3,46 +3,127 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'test1',
   template: `
-  <div class="row">
-  <div class="col-12" *ngFor="let question of questions; let qi = index;">
-  <div class = "card mt-2 mb-2">
-    <div class="card-header">
-      {{question.statement}}
-    </div>
-    <div class="card-body">
-      <label>
-        <input type="radio" value="1" name="option_{{qi}}" [(ngModel)]="question.userSelectedOption"/>
-        {{question.option1}}
-      </label>
-    </div>
-    <div class="card-body">
-      <label>  
-        <input type="radio" value="2" name="option_{{qi}}">
-        {{question.option2}}
-      </label>
-    </div>
-    <div class="card-body">
-      <label>
-        <input type="radio" value="3" name="option_{{qi}}">
-        {{question.option3}}
-      </label>
+    <div class="row">
+    <div class="col-12" *ngFor="let question of questions; let qi = index;">
+    <div class = "card mt-2 mb-2">
+      <div class="card-header">
+        {{question.statement}}
       </div>
-    <div class="card-body">
-      <label>
-        <input type="radio" value="4" name="option_{{qi}}">
-        {{question.option4}}
-      </label>
+      <div class="card-body">
+        <label>
+          <input type="radio" [value]="1" name="option_{{qi}}" [(ngModel)]="question.userSelectedOption"
+          [disabled] = "isTestOver"/>
+          {{question.option1}}
+          <span *ngIf="isTestOver">
+            <span *ngIf="question.correctAnswer == 1" >
+              Correct - +{{question.marks}}
+            </span>
+
+            <span *ngIf="question.correctAnswer != 1">
+              Wrong: -{{question.marks}}
+            </span>
+
+            <span *ngIf="question.userSelectedOption == 0">
+              Not Attempted. 0 Marks. 
+            </span>
+          </span>
+        </label>
+      </div>
+      <div class="card-body">
+        <label>  
+          <input type="radio" [value]="2" name="option_{{qi}}" [(ngModel)]="question.userSelectedOption">
+          {{question.option2}}
+          <span *ngIf="isTestOver">
+            <span *ngIf="question.correctAnswer == 2" >
+              Correct - +{{question.marks}}
+            </span>
+
+            <span *ngIf="question.correctAnswer != 2">
+              Wrong: -{{question.marks}}
+            </span>
+
+            <span *ngIf="question.userSelectedOption == 0">
+              Not Attempted. 0 Marks. 
+            </span>
+          </span>
+        </label>
+      </div>
+      <div class="card-body">
+        <label>
+          <input type="radio" [value]="3" name="option_{{qi}}" [(ngModel)]="question.userSelectedOption">
+          {{question.option3}}
+          <span *ngIf="isTestOver">
+            <span *ngIf="question.correctAnswer == 3" >
+              Correct - +{{question.marks}}
+            </span>
+
+            <span *ngIf="question.correctAnswer != 3">
+              Wrong: -{{question.marks}}
+            </span>
+
+            <span *ngIf="question.userSelectedOption == 3">
+              Not Attempted. 0 Marks. 
+            </span>
+          </span>
+        </label>
+        </div>
+      <div class="card-body">
+        <label>
+          <input type="radio" [value]="4" name="option_{{qi}}" [(ngModel)]="question.userSelectedOption">
+          {{question.option4}}
+          <span *ngIf="isTestOver">
+            <span *ngIf="question.correctAnswer == 4" >
+              Correct - +{{question.marks}}
+            </span>
+
+            <span *ngIf="question.correctAnswer != 4">
+              Wrong: -{{question.marks}}
+            </span>
+
+            <span *ngIf="question.userSelectedOption == 0">
+              Not Attempted. 0 Marks. 
+            </span>
+          </span>
+        </label>
+      </div>
+
+      <div class="card-footer">
+        Marks: {{question.marks}}
+      </div>
+    </div>
+    </div>
+    
+    <div class = "row mt-2 mb-5" *ngIf="!isTestOver">
+      <div class="col-12">
+        <button class="btn btn-primary" (click)="finish()">Finish</button>
+      </div>
     </div>
 
-    <div class="card-footer">
-      Marks: {{question.marks}}
+    <div class = "row mt-2 mb-5">
+      <div class="col-12">
+        You obtained {{userMarks}} out of {{totalMarks}}
+      </div>
     </div>
-  </div>
-  </div>
-  </div>
+
+    </div>
   `,
 })
 export class Test1Component {
+  public isTestOver = false;
+  public userMarks = 0;
+  public totalMarks = 0;
+
+  finish() {
+    for (let question of this.questions) {
+      if (question.correctAnswer === question.userSelectedOption) {
+        this.userMarks += question.marks;
+      }
+      this.totalMarks += question.marks;
+    }
+
+    this.isTestOver = true;
+  }
+
   public questions = [
     {
       statement: 'AAA',
@@ -63,7 +144,6 @@ export class Test1Component {
       correctAnswer: 1,
       marks: 5,
       userSelectedOption: 0,
-
     },
     {
       statement: 'CCC',
